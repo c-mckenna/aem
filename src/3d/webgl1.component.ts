c68e4e2fe172439266b5d2a75a03ec6f51d9292f
Callum McKenna <contact@callum.id.au>
2019-05-21 22:04:42 +1000

    Fix unpack alignment

--- src/3d/webgl1.component.ts
+++ src/3d/webgl1.component.ts
@@ -145,7 +145,7 @@ void main() {
     gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
     gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
 
-    gl.pixelStorei(gl.UNPACK_ALIGNMENT, 4);
+    gl.pixelStorei(gl.UNPACK_ALIGNMENT, 1);
     gl.texImage2D(gl.TEXTURE_2D, 0, gl.LUMINANCE, this.width, this.height, 0, gl.LUMINANCE, gl.FLOAT, this.data);
     gl.bindTexture(gl.TEXTURE_2D, null);
 
